42a0bff17011e87ebada116a44915272b23b2b92
wpy <wpy@172.31.98.197>
2016-07-11 18:40:57 -0400

    Migrated

---
+++ client/main.js
@@ -0,0 +1,199 @@
+/**
+ * Main game logic code
+ */
+
+
+var ctx = document.getElementById("ctx").getContext("2d");
+
+// Define global variables
+var HEIGHT = 600;
+var WIDTH = 1200;
+var robotCount = 0;
+var GRAVITY = 0;
+var pause = false;
+
+// Get all images
+var imgBg = new Image();
+imgBg.src = "img/agario_bg.jpg";
+
+// Create player object
+function Player(posX, posY, speedX, speedY, absSpeed, name, color, mass, radius) {
+	this.posX = posX;
+	this.posY = posY;
+	this.speedX = speedX;
+	this.speedY = speedY;
+	this.absSpeed = absSpeed;
+	this.name = name;
+	this.color = color;
+	this.mass = mass;
+	this.radius = radius;
+	
+	this.update = function() {
+		var actualX = WIDTH / 2;
+		var actualY = HEIGHT / 2;
+		
+		// Adjust speed by mouse location
+		var dist = distance(mouseX - actualX, mouseY - actualY);
+		var ratioX = (mouseX - actualX) / dist;
+		var ratioY = (mouseY - actualY) / dist;
+		
+		// Avoid shaking if object is not moving
+		if (dist < player.absSpeed) dist = 0;
+		
+		if (dist === 0) {
+			this.speedX = 0;
+			this.speedY = 0;
+		} else {
+			this.speedX = this.absSpeed * ratioX;
+			this.speedY = this.absSpeed * ratioY;
+		}
+		//console.log('mouseX=' + mouseX + '|posX=' + this.posX + '|dist=' + dist);
+		
+		// Calculate radius by mass
+		this.radius = Math.round(Math.sqrt(this.mass * 10 / Math.PI));
+		
+		this.posX = Math.round(this.posX + this.speedX);
+		this.posY = Math.round(this.posY + this.speedY);
+		
+		// Make sure object doesn't go out of the boundary
+		if (this.posX >= imgBg.width - this.radius) this.posX = imgBg.width - this.radius;
+		if (this.posX <= this.radius) this.posX = this.radius;
+		if (this.posY >= imgBg.height - this.radius) this.posY = imgBg.height - this.radius;
+		if (this.posY <= this.radius) this.posY = this.radius;
+		if ((this.posX >= imgBg.width - this.radius) || (this.posX <= this.radius) || (this.posY >= imgBg.height - this.radius) || (this.posY <= this.radius)) {
+			console.log("Boundary reached");
+		}
+	}
+	
+}
+
+// Constructor for computer robot object
+function Robot(id, posX, posY, speedX, speedY, absSpeed, color, mass, name) {
+	this.id = id;
+	this.posX = Math.round(posX);
+	this.posY = Math.round(posY);
+	this.speedX = Math.round(speedX);
+	this.speedY = Math.round(speedY);
+	this.absSpeed = absSpeed;
+	this.color = color;
+	this.mass = mass;
+	this.name = name + id;
+	
+	this.update = function() {
+		
+		// Calculate radius by mass
+		this.radius = Math.round(Math.sqrt(this.mass * 10 / Math.PI));
+
+		this.posX = Math.round(this.posX + this.speedX);
+		this.posY = Math.round(this.posY + this.speedY);
+		
+		// Make sure object doesn't go out of the boundary
+		if (this.posX >= imgBg.width - this.radius) {
+			this.posX = imgBg.width - this.radius;
+			this.speedX = -this.speedX;
+		}
+		if (this.posX <= this.radius) {
+			this.posX = this.radius;
+			this.speedX = -this.speedX;
+		}
+		if (this.posY >= imgBg.height - this.radius) {
+			this.posY = imgBg.height - this.radius;
+			this.speedY = -this.speedY;
+		}
+		if (this.posY <= this.radius) {
+			this.posY = this.radius;
+			this.speedY = -this.speedY;
+		}
+	}
+}
+
+
+function drawEntity(entity, player) {
+	
+	var actualX = entity.posX - player.posX;
+	var actualY = entity.posY - player.posY;
+	actualX += WIDTH / 2;
+	actualY += HEIGHT / 2;
+	
+	ctx.beginPath();
+	ctx.arc(actualX, actualY, entity.radius, 0, 2 * Math.PI, false);
+	ctx.fillStyle = entity.color;
+	ctx.fill();
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = '#003300';
+	ctx.stroke();
+	
+	ctx.font = '15px Arial';
+	var nameX = actualX - ctx.measureText(entity.name).width / 2;
+	var nameY = actualY;
+	ctx.fillStyle = 'black';
+	ctx.fillText(entity.name, nameX, nameY);
+}
+
+function distance(x, y) {
+	var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	return root;
+}
+
+function robotIsEatable(player, robot) {
+	return distance(player.posX - robot.posX, player.posY - robot.posY) < player.radius && player.mass > robot.mass
+}
+
+function drawBackground() {
+	var x = WIDTH - player.posX;
+	var y = HEIGHT - player.posY;
+	ctx.drawImage(imgBg, WIDTH, HEIGHT, imgBg.width, imgBg.height, x, y, imgBg.width, imgBg.height);
+}
+
+/*
+ * Main logic
+ */
+var robotList = {};
+var player = new Player(Math.round(Math.random() * WIDTH), Math.round(Math.random() * HEIGHT), 0, 0, 4, "Hadoop", 'green', 50, 10);
+//Create robot objects
+
+
+
+function createRobot() {
+	robotList[robotCount] = new Robot(robotCount, Math.random() * WIDTH, Math.random() * HEIGHT, Math.random() * 10, Math.random() * 10, 6, 'grey', 300, "Spark");
+	robotCount += 1;
+	console.log("Robot count: " + robotCount);
+}
+
+//Robot("Spark", Math.random() * WIDTH, Math.random() * HEIGHT, Math.random() * 10, Math.random() * 10, 6, 'yellow', 500, 10);
+//Robot("HBase", Math.random() * WIDTH, Math.random() * HEIGHT, Math.random() * 10, Math.random() * 10, 6, 'red', 750, 10);
+
+
+//Track the mouse location
+var mouseX = 0;
+var mouseY = 0;
+document.onmousemove = function(mouse) {
+	mouseX = Math.round(mouse.clientX);
+	mouseY = Math.round(mouse.clientY);
+}
+
+document.onclick = function(mouse) {
+	player.mass = player.mass * 2;
+}
+
+
+function updateUI() {
+
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	drawBackground();
+	for (var key in robotList){
+		var bot = robotList[key];
+		if (robotIsEatable(player, bot)) {
+			player.mass += bot.mass;
+			delete robotList[key];
+			//console.log("One bot is eaten");
+		}
+		bot.update();
+		drawEntity(bot, player);
+	}
+	player.update();
+	drawEntity(player, player);
+}
+
+setInterval(updateUI, 20);
+setInterval(createRobot, 5000);
---
+++ server.js
@@ -0,0 +1,175 @@
+function PlayerData(id, x, y, speed, mass, name, color) {
+	this.id = id;
+	this.posX = x;
+	this.posY = y;
+	this.speed = speed;
+	this.mass = mass;
+	this.name = name;
+	this.color = color;
+	
+	// Zero the speed when initializing
+	this.speedX = 0;
+	this.speedY = 0;
+	
+	this.update = function() {
+		//console.log(this.speedX + '|' + this.speedY);
+
+		// Calculate radius by mass
+		this.radius = Math.round(Math.sqrt(this.mass * 10 / Math.PI));
+		
+		this.posX = Math.round(this.posX + this.speedX);
+		this.posY = Math.round(this.posY + this.speedY);
+		
+		// Make sure object doesn't go out of the boundary
+		if (this.posX >= WORLD_WIDTH - this.radius) this.posX = WORLD_WIDTH - this.radius;
+		if (this.posX <= this.radius) this.posX = this.radius;
+		if (this.posY >= WORLD_HEIGHT - this.radius) this.posY = WORLD_HEIGHT - this.radius;
+		if (this.posY <= this.radius) this.posY = this.radius;
+		if ((this.posX >= WORLD_WIDTH - this.radius) || (this.posX <= this.radius) || (this.posY >= WORLD_HEIGHT - this.radius) || (this.posY <= this.radius)) {
+			console.log("Boundary reached");
+		}
+	}
+	
+	this.updateSpeed = function(speedX, speedY) {
+		//console.log("speed is updated");
+		this.speedX = speedX;
+		this.speedY = speedY;
+	}
+	
+	this.addMass = function(delta) {
+		this.mass += delta;
+	}
+}
+
+function Food(id, x, y) {
+	this.id = id;
+	this.posX = x;
+	this.posY = y;
+	this.radius = 10;
+}
+
+var WORLD_WIDTH = 2000;
+var WORLD_HEIGHT = 1000;
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+// Anonomous function
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+
+server.listen(2000);
+console.log("Server started");
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+var FOOD_LIST = {};
+var updatePack = [];
+var deletePack = [];
+var foodPack = [];
+var connectionId = 0;
+var foodId = 0;
+
+var io = require('socket.io')(server, {});
+io.sockets.on('connection', function(socket){
+	console.log('socket connection');
+	var address = socket.handshake.address;
+	//var id = address + '.' + Math.abs(Math.round(Math.random() * 1000));
+	var id = connectionId ++;
+	socket.id = id;
+	socket.x = 0;
+	socket.y = 0;
+	SOCKET_LIST[id] = socket;
+	PLAYER_LIST[id] = new PlayerData(id, 100, 100, 5, 100, 'Hadoop', 'green');
+	socket.emit('init', {
+		player: PLAYER_LIST[id],
+		worldWidth: WORLD_WIDTH,
+		worldHeight: WORLD_HEIGHT
+	});
+	
+	for (var i in FOOD_LIST) {
+		socket.emit('addFood', FOOD_LIST[i]);
+	}
+	
+	socket.on('disconnect', function() {
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+		deletePack.push(socket.id);
+	});
+	
+	socket.on('updateSpeed', function(data) {
+		PLAYER_LIST[socket.id].updateSpeed(data.speedX, data.speedY);
+	});
+});
+
+function emitPackets() {
+	for (var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		PLAYER_LIST[i].update();
+		
+		// See if any player can eat
+		eatNearbyFood(PLAYER_LIST[i]);
+		updatePack.push(PLAYER_LIST[i]);
+	}
+
+	propogatePacket('update', updatePack);
+	propogatePacket('delete', deletePack);
+	
+	// clear packets
+	updatePack = [];
+	deletePack = [];
+}
+
+function generateFood() {
+	var food = new Food(foodId, Math.round(Math.random() * WORLD_WIDTH), Math.round(Math.random() * WORLD_HEIGHT));
+	FOOD_LIST[foodId] = food;
+	foodId ++;
+	propogatePacket('addFood', food);
+}
+
+function enemyIsEatable(player, enemy) {
+	return distance(player.posX - enemy.posX, player.posY - enemy.posY) < player.radius && player.mass > enemy.mass;
+}
+
+function distance(x, y) {
+	var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	return root;
+}
+
+function eatNearbyFood(player) {
+	for (var i in FOOD_LIST) {
+		var food = FOOD_LIST[i];
+		if (distance(player.posX - food.posX, player.posY - food.posY) <= player.radius - food.radius) {
+			player.addMass(5);
+			delete FOOD_LIST[i];
+			propogatePacket('deleteFood', food.id);
+		}
+	}
+}
+
+
+function propogatePacket(message, packet) {
+	for (var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit(message, packet);
+	}
+}
+
+setInterval(emitPackets, 1000/30);
+setInterval(generateFood, 1000);
+
+///////////////// DEBUGGING //////////////
+
+function getMapSize(map) {
+	var count = 0;
+	for (var key in map) {
+		count ++;
+	}
+	return count;
+}
+
+function printPlayer(player) {
+	console.log(player.name + '|' + player.posX + '|' + player.posY + '|' + player.mass);
+}
