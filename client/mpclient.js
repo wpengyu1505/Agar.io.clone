dbf0d9b00b0857e3e7dc087382d567cbb7310374
wpy <wpy@10.0.0.240>
2016-07-13 22:20:13 -0400

    Formatted the js code

--- client/mpclient.js
+++ client/mpclient.js
@@ -30,191 +30,196 @@ imgBg.src = "client/img/agario_bg.jpg";
 
 // Create player object
 function Player(data) {
-	this.id = data.id;
-	this.posX = data.posX;
-	this.posY = data.posY;
-	this.absSpeed = data.speed;
-	this.name = data.name;
-	this.color = data.color;
-	this.mass = data.mass;
-	this.radius = getRadiusByMass(this.mass);
-	
-	this.update = function(data) {
-		this.posX = data.posX;
-		this.posY = data.posY;
-		this.mass = data.mass;
-		this.radius = getRadiusByMass(this.mass);
-	}
-	
-	this.updateSpeed = function() {
-		var actualX = WIDTH / 2;
-		var actualY = HEIGHT / 2;
-		
-		// Adjust speed by mouse location
-		var dist = distance(mouseX - actualX, mouseY - actualY);
-		var ratioX = (mouseX - actualX) / dist;
-		var ratioY = (mouseY - actualY) / dist;
-		
-		// Avoid shaking if object is not moving
-		if (dist < player.absSpeed) dist = 0;
-		
-		if (dist === 0) {
-			this.speedX = 0;
-			this.speedY = 0;
-		} else {
-			this.speedX = this.absSpeed * ratioX;
-			this.speedY = this.absSpeed * ratioY;
-		}
-		
-		return {
-			speedX: this.speedX,
-			speedY: this.speedY
-		};
-	}
+    this.id = data.id;
+    this.posX = data.posX;
+    this.posY = data.posY;
+    this.absSpeed = data.speed;
+    this.name = data.name;
+    this.color = data.color;
+    this.mass = data.mass;
+    this.radius = getRadiusByMass(this.mass);
+
+    this.update = function(data) {
+        this.posX = data.posX;
+        this.posY = data.posY;
+        this.mass = data.mass;
+        this.radius = getRadiusByMass(this.mass);
+    }
+
+    this.updateSpeed = function() {
+        var actualX = WIDTH / 2;
+        var actualY = HEIGHT / 2;
+
+        // Adjust speed by mouse location
+        var dist = distance(mouseX - actualX, mouseY - actualY);
+        var ratioX = (mouseX - actualX) / dist;
+        var ratioY = (mouseY - actualY) / dist;
+
+        // Avoid shaking if object is not moving
+        if (dist < player.absSpeed)
+            dist = 0;
+
+        if (dist === 0) {
+            this.speedX = 0;
+            this.speedY = 0;
+        } else {
+            this.speedX = this.absSpeed * ratioX;
+            this.speedY = this.absSpeed * ratioY;
+        }
+
+        return {
+            speedX : this.speedX,
+            speedY : this.speedY
+        };
+    }
 }
 
 function Enemy(data) {
-	this.posX = data.posX;
-	this.posY = data.posY;
-	this.speed = data.speed;
-	this.mass = data.mass;
-	this.name = data.name;
-	this.color = data.color;
-	
-	this.update = function(data) {
-		this.posX = data.posX;
-		this.posY = data.posY;
-		this.speed = data.speed;
-		this.mass = data.mass;
-		this.name = data.name;
-		this.color = data.color;
-	}
+    this.posX = data.posX;
+    this.posY = data.posY;
+    this.speed = data.speed;
+    this.mass = data.mass;
+    this.name = data.name;
+    this.color = data.color;
+
+    this.update = function(data) {
+        this.posX = data.posX;
+        this.posY = data.posY;
+        this.speed = data.speed;
+        this.mass = data.mass;
+        this.name = data.name;
+        this.color = data.color;
+    }
 }
 
 function Food(data) {
-	this.id = data.id;
-	this.posX = data.posX;
-	this.posY = data.posY;
-	this.color = getRandomColor();
-	this.radius = 10;
+    this.id = data.id;
+    this.posX = data.posX;
+    this.posY = data.posY;
+    this.color = getRandomColor();
+    this.radius = 10;
 }
 
 function drawEntity(entity, player) {
-	
-	var actualX = entity.posX - player.posX;
-	var actualY = entity.posY - player.posY;
-	actualX += WIDTH / 2;
-	actualY += HEIGHT / 2;
-	
-	context.beginPath();
-	context.arc(actualX, actualY, entity.radius, 0, 2 * Math.PI, false);
-	context.fillStyle = entity.color;
-	context.fill();
-	context.lineWidth = 1;
-	context.strokeStyle = '#003300';
-	context.stroke();
-	
-	context.font = '15px Arial';
-	if (typeof entity.name != 'undefined') {
-		var nameX = actualX - context.measureText(entity.name).width / 2;
-		var nameY = actualY;
-		context.fillStyle = 'black';
-		context.fillText(entity.name, nameX, nameY);
-	}
+
+    var actualX = entity.posX - player.posX;
+    var actualY = entity.posY - player.posY;
+    actualX += WIDTH / 2;
+    actualY += HEIGHT / 2;
+
+    context.beginPath();
+    context.arc(actualX, actualY, entity.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = entity.color;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = '#003300';
+    context.stroke();
+
+    context.font = '15px Arial';
+    if (typeof entity.name != 'undefined') {
+        var nameX = actualX - context.measureText(entity.name).width / 2;
+        var nameY = actualY;
+        context.fillStyle = 'black';
+        context.fillText(entity.name, nameX, nameY);
+    }
 }
 
 function distance(x, y) {
-	var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-	return root;
+    var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return root;
 }
 
 function getRadiusByMass(mass) {
-	return Math.round(Math.sqrt(mass * 10 / Math.PI));
+    return Math.round(Math.sqrt(mass * 10 / Math.PI));
 }
 
 function drawBackground() {
-	var x = WIDTH - player.posX;
-	var y = HEIGHT - player.posY;
-	context.drawImage(imgBg, WIDTH, HEIGHT, imgBg.width, imgBg.height, x, y, imgBg.width, imgBg.height);
+    var x = WIDTH - player.posX;
+    var y = HEIGHT - player.posY;
+    context.drawImage(imgBg, WIDTH, HEIGHT, imgBg.width, imgBg.height, x, y,
+            imgBg.width, imgBg.height);
 }
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 
-///////////////////////// Main logic goes here ////////////////////////
+// /////////////////////// Main logic goes here ////////////////////////
 
 var player;
 var onlinePlayerList = {};
 var foodList = {};
 var socket = io();
 
-submitButton.onclick = function(){
-    socket.emit('signIn',{name:nameText.value,color:colorText.value});
+submitButton.onclick = function() {
+    socket.emit('signIn', {
+        name : nameText.value,
+        color : colorText.value
+    });
 }
 
 socket.on('init', function(data) {
-	player = new Player(data.player);
-	ID = player.id;
-	worldWidth = data.worldWidth;
-	worldHeight = data.worldHeight;
-	console.log("Player initialized " + ID);
-	
-	document.onmousemove = function(mouse) {
-		mouseX = Math.round(mouse.clientX);
-		mouseY = Math.round(mouse.clientY);
-		socket.emit('updateSpeed', player.updateSpeed());
-	}
-	
+    player = new Player(data.player);
+    ID = player.id;
+    worldWidth = data.worldWidth;
+    worldHeight = data.worldHeight;
+    console.log("Player initialized " + ID);
+
+    document.onmousemove = function(mouse) {
+        mouseX = Math.round(mouse.clientX);
+        mouseY = Math.round(mouse.clientY);
+        socket.emit('updateSpeed', player.updateSpeed());
+    }
+
     signDiv.style.display = 'none';
     gameDiv.style.display = 'inline-block';
 
 });
 
 socket.on('update', function(data) {
-	context.clearRect(0, 0, WIDTH, HEIGHT);
-	//console.log('Online player: ' + getMapSize(onlinePlayerList));
-	drawBackground();
-	for (var i = 0; i < data.length; i ++) {
-		var id = data[i].id;
-		onlinePlayerList[id] = data[i];
-		//console.log(id + '|' + ID);
-		if (id === ID) {
-			player.update(data[i]);
-		}
-	}
-	
-	// Draw food first so it's the bottom
-	for (var i in foodList) {
-		drawEntity(foodList[i], player);
-	}
-	
-	//printPlayer(player);
-	for (var i in onlinePlayerList) {
-		drawEntity(onlinePlayerList[i], player);
-	}
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+    // console.log('Online player: ' + getMapSize(onlinePlayerList));
+    drawBackground();
+    for (var i = 0; i < data.length; i++) {
+        var id = data[i].id;
+        onlinePlayerList[id] = data[i];
+        // console.log(id + '|' + ID);
+        if (id === ID) {
+            player.update(data[i]);
+        }
+    }
+
+    // Draw food first so it's the bottom
+    for ( var i in foodList) {
+        drawEntity(foodList[i], player);
+    }
+
+    // printPlayer(player);
+    for ( var i in onlinePlayerList) {
+        drawEntity(onlinePlayerList[i], player);
+    }
 
 });
 
 socket.on('delete', function(data) {
-	//console.log('delete is received');
-	delete onlinePlayerList[data];
+    // console.log('delete is received');
+    delete onlinePlayerList[data];
 });
 
 socket.on('addFood', function(data) {
-	console.log('food is here');
-	var food = new Food(data);
-	foodList[food.id] = food;
+    console.log('food is here');
+    var food = new Food(data);
+    foodList[food.id] = food;
 });
 
 socket.on('deleteFood', function(data) {
-	console.log('food is deleted');
-	delete foodList[data];
+    console.log('food is deleted');
+    delete foodList[data];
 });
 
 //Track the mouse location
@@ -231,13 +236,14 @@ var mouseY = 0;
 ///////////////// DEBUGGING //////////////
 
 function getMapSize(map) {
-	var count = 0;
-	for (var key in map) {
-		count ++;
-	}
-	return count;
+    var count = 0;
+    for ( var key in map) {
+        count++;
+    }
+    return count;
 }
 
 function printPlayer(player) {
-	console.log(player.name + '|' + player.posX + '|' + player.posY + '|' + player.mass);
+    console.log(player.name + '|' + player.posX + '|' + player.posY + '|'
+            + player.mass);
 }
--- server.js
+++ server.js
@@ -1,53 +1,57 @@
 function PlayerData(id, x, y, speed, mass, name, color) {
-	this.id = id;
-	this.posX = x;
-	this.posY = y;
-	this.speed = speed;
-	this.mass = mass;
-	this.name = name;
-	this.color = color;
-	this.alive = true;
-	
-	// Zero the speed when initializing
-	this.speedX = 0;
-	this.speedY = 0;
-	
-	this.update = function() {
-		//console.log(this.speedX + '|' + this.speedY);
+    this.id = id;
+    this.posX = x;
+    this.posY = y;
+    this.speed = speed;
+    this.mass = mass;
+    this.name = name;
+    this.color = color;
+    this.alive = true;
 
-		// Calculate radius by mass
-		this.radius = Math.round(Math.sqrt(this.mass * 10 / Math.PI));
-		
-		this.posX = Math.round(this.posX + this.speedX);
-		this.posY = Math.round(this.posY + this.speedY);
-		
-		// Make sure object doesn't go out of the boundary
-		if (this.posX >= WORLD_WIDTH - this.radius) this.posX = WORLD_WIDTH - this.radius;
-		if (this.posX <= this.radius) this.posX = this.radius;
-		if (this.posY >= WORLD_HEIGHT - this.radius) this.posY = WORLD_HEIGHT - this.radius;
-		if (this.posY <= this.radius) this.posY = this.radius;
-	}
+    // Zero the speed when initializing
+    this.speedX = 0;
+    this.speedY = 0;
 
-	this.updateSpeed = function(speedX, speedY) {
-		//console.log("speed is updated");
-		this.speedX = speedX;
-		this.speedY = speedY;
-	}
-	
-	this.addMass = function(delta) {
-		this.mass += delta;
-	}	
-	
-	this.die = function() {
-		this.alive = false;
-	}
+    this.update = function() {
+        // console.log(this.speedX + '|' + this.speedY);
+
+        // Calculate radius by mass
+        this.radius = Math.round(Math.sqrt(this.mass * 10 / Math.PI));
+
+        this.posX = Math.round(this.posX + this.speedX);
+        this.posY = Math.round(this.posY + this.speedY);
+
+        // Make sure object doesn't go out of the boundary
+        if (this.posX >= WORLD_WIDTH - this.radius)
+            this.posX = WORLD_WIDTH - this.radius;
+        if (this.posX <= this.radius)
+            this.posX = this.radius;
+        if (this.posY >= WORLD_HEIGHT - this.radius)
+            this.posY = WORLD_HEIGHT - this.radius;
+        if (this.posY <= this.radius)
+            this.posY = this.radius;
+    }
+
+    this.updateSpeed = function(speedX, speedY) {
+        // console.log("speed is updated");
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    this.addMass = function(delta) {
+        this.mass += delta;
+    }
+
+    this.die = function() {
+        this.alive = false;
+    }
 }
 
 function Food(id, x, y) {
-	this.id = id;
-	this.posX = x;
-	this.posY = y;
-	this.radius = 10;
+    this.id = id;
+    this.posX = x;
+    this.posY = y;
+    this.radius = 10;
 }
 
 var WORLD_WIDTH = 2000;
@@ -58,7 +62,7 @@ var server = require('http').Server(app);
 
 // Anonomous function
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
@@ -75,121 +79,127 @@ var connectionId = 0;
 var foodId = 0;
 
 var io = require('socket.io')(server, {});
-io.sockets.on('connection', function(socket){
-	console.log('socket connection');
-	var address = socket.handshake.address;
-	//var id = address + '.' + Math.abs(Math.round(Math.random() * 1000));
-	var id = connectionId ++;
-	socket.id = id;
-	socket.on('signIn', function(data) {
-		SOCKET_LIST[id] = socket;
-		PLAYER_LIST[id] = new PlayerData(id, 100, 100, 5, 100, data.name, data.color);
-		socket.emit('init', {
-			player: PLAYER_LIST[id],
-			worldWidth: WORLD_WIDTH,
-			worldHeight: WORLD_HEIGHT
-		});
-		
-		for (var i in FOOD_LIST) {
-			socket.emit('addFood', FOOD_LIST[i]);
-		}
-		
-		socket.on('disconnect', function() {
-			delete SOCKET_LIST[socket.id];
-			delete PLAYER_LIST[socket.id];
-			deletePack.push(socket.id);
-		});
-		
-		socket.on('updateSpeed', function(data) {
-			PLAYER_LIST[socket.id].updateSpeed(data.speedX, data.speedY);
-		});		
-	});
+io.sockets.on('connection', function(socket) {
+    console.log('socket connection');
+    var address = socket.handshake.address;
+    // var id = address + '.' + Math.abs(Math.round(Math.random() * 1000));
+    var id = connectionId++;
+    socket.id = id;
+    socket.on('signIn', function(data) {
+        SOCKET_LIST[id] = socket;
+        PLAYER_LIST[id] = new PlayerData(id, 100, 100, 5, 100, data.name,
+                data.color);
+        socket.emit('init', {
+            player : PLAYER_LIST[id],
+            worldWidth : WORLD_WIDTH,
+            worldHeight : WORLD_HEIGHT
+        });
+
+        for ( var i in FOOD_LIST) {
+            socket.emit('addFood', FOOD_LIST[i]);
+        }
+
+        socket.on('disconnect', function() {
+            delete SOCKET_LIST[socket.id];
+            delete PLAYER_LIST[socket.id];
+            deletePack.push(socket.id);
+        });
+
+        socket.on('updateSpeed', function(data) {
+            PLAYER_LIST[socket.id].updateSpeed(data.speedX, data.speedY);
+        });
+    });
 
 });
 
 function emitPackets() {
-	for (var i in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		if (PLAYER_LIST[i].alive) {
-			PLAYER_LIST[i].update();
-			// See if any player can eat
-			eatNearbyFood(PLAYER_LIST[i]);
-			prepareToEatOthers(PLAYER_LIST[i]);
-			updatePack.push(PLAYER_LIST[i]);
-			
-		} else {
-			delete SOCKET_LIST[i];
-			delete PLAYER_LIST[i];
-			deletePack.push(i);
-		}
-	}
+    for ( var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        if (PLAYER_LIST[i].alive) {
+            PLAYER_LIST[i].update();
+            // See if any player can eat
+            eatNearbyFood(PLAYER_LIST[i]);
+            prepareToEatOthers(PLAYER_LIST[i]);
+            updatePack.push(PLAYER_LIST[i]);
 
-	propogatePacket('update', updatePack);
-	propogatePacket('delete', deletePack);
-	
-	// clear packets
-	updatePack = [];
-	deletePack = [];
+        } else {
+            delete SOCKET_LIST[i];
+            delete PLAYER_LIST[i];
+            deletePack.push(i);
+        }
+    }
+
+    propogatePacket('update', updatePack);
+    propogatePacket('delete', deletePack);
+
+    // clear packets
+    updatePack = [];
+    deletePack = [];
 }
 
 function generateFood() {
-	var food = new Food(foodId, Math.round(Math.random() * WORLD_WIDTH), Math.round(Math.random() * WORLD_HEIGHT));
-	FOOD_LIST[foodId] = food;
-	foodId ++;
-	propogatePacket('addFood', food);
+    var food = new Food(foodId, Math.round(Math.random() * WORLD_WIDTH), Math
+            .round(Math.random() * WORLD_HEIGHT));
+    FOOD_LIST[foodId] = food;
+    foodId++;
+    propogatePacket('addFood', food);
 }
 
 function enemyIsEatable(player, enemy) {
-	return distance(player.posX - enemy.posX, player.posY - enemy.posY) < player.radius && player.mass > enemy.mass;
+    return distance(player.posX - enemy.posX, player.posY - enemy.posY) < player.radius
+            && player.mass > enemy.mass;
 }
 
 function distance(x, y) {
-	var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-	return root;
+    var root = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    return root;
 }
 
 function eatNearbyFood(player) {
-	for (var i in FOOD_LIST) {
-		var food = FOOD_LIST[i];
-		if (distance(player.posX - food.posX, player.posY - food.posY) <= player.radius - food.radius) {
-			player.addMass(5);
-			delete FOOD_LIST[i];
-			propogatePacket('deleteFood', food.id);
-		}
-	}
+    for ( var i in FOOD_LIST) {
+        var food = FOOD_LIST[i];
+        if (distance(player.posX - food.posX, player.posY - food.posY) <= player.radius
+                - food.radius) {
+            player.addMass(5);
+            delete FOOD_LIST[i];
+            propogatePacket('deleteFood', food.id);
+        }
+    }
 }
 
 function prepareToEatOthers(player) {
-	for (var i in PLAYER_LIST) {
-		var enemy = PLAYER_LIST[i];
-		if (enemy.id !== player.id && enemy.alive && enemyIsEatable(player, enemy)) {
-			enemy.die();
-			player.addMass(enemy.mass);
-			console.log('Player ' + enemy.id + ' is eaten');
-		}
-	}
+    for ( var i in PLAYER_LIST) {
+        var enemy = PLAYER_LIST[i];
+        if (enemy.id !== player.id && enemy.alive
+                && enemyIsEatable(player, enemy)) {
+            enemy.die();
+            player.addMass(enemy.mass);
+            console.log('Player ' + enemy.id + ' is eaten');
+        }
+    }
 }
 
 function propogatePacket(message, packet) {
-	for (var i in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		socket.emit(message, packet);
-	}
+    for ( var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.emit(message, packet);
+    }
 }
 
-setInterval(emitPackets, 1000/30);
+setInterval(emitPackets, 1000 / 30);
 setInterval(generateFood, 1000);
 
 ///////////////// DEBUGGING //////////////
 
 function getMapSize(map) {
-	var count = 0;
-	for (var key in map) {
-		count ++;
-	}
-	return count;
+    var count = 0;
+    for ( var key in map) {
+        count++;
+    }
+    return count;
 }
 
 function printPlayer(player) {
-	console.log(player.name + '|' + player.posX + '|' + player.posY + '|' + player.mass);
+    console.log(player.name + '|' + player.posX + '|' + player.posY + '|'
+            + player.mass);
 }
